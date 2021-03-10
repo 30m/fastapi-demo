9d698c068cb8f07589cb5b8a5b2d52ceba3d495c
30m <e165414k@st.u-gakugei.ac.jp>
2021-03-10 13:44:08 +0900

    🚧 finish part3

--- backend/app/api/routes/__init__.py
+++ backend/app/api/routes/__init__.py
@@ -1,5 +1,7 @@
 from fastapi import APIRouter
-from app.api.routes.hedgehogs import routor as hedgehogs_router
+from app.api.routes.hedgehogs import router as hedgehogs_router
+
 
 router = APIRouter()
-router.include_router(hedgehogs_router,prefix="/hedgehogs",tags=["hedgehogs"])
+router.include_router(
+    hedgehogs_router, prefix="/hedgehogs", tags=["hedgehogs"])
